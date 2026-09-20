0478693de82c0210399dcdb5bcb2db91c0d261cb
Snehal Bedage <snehalbedage3630@gmail.com>
2026-09-20 19:12:23 +0530

    Complete Week 4 Day 4 - Database Integration

--- task_tracker/models.py
+++ task_tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=20, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
