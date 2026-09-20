43296b1362ebb3a1155909d4140ea14709c837bc
Snehal Bedage <snehalbedage3630@gmail.com>
2026-09-20 19:44:17 +0530

    Complete Week 4 Day 5 - REST API

--- task_tracker/views.py
+++ task_tracker/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import Task
 import json
 
 
@@ -13,30 +14,83 @@ def home(request):
 def tasks(request):
 
     if request.method == 'GET':
+        task_list = Task.objects.all()
+
+        data = []
+
+        for task in task_list:
+            data.append({
+                "id": task.id,
+                "title": task.title,
+                "description": task.description,
+                "status": task.status,
+                "created_at": task.created_at,
+            })
+
         return JsonResponse({
-            "message": "GET request received"
+            "tasks": data
         })
 
     elif request.method == 'POST':
         data = json.loads(request.body)
 
+        task = Task.objects.create(
+            title=data.get('title'),
+            description=data.get('description', ''),
+            status=data.get('status', 'Pending')
+        )
+
         return JsonResponse({
-            "message": "POST request received",
-            "data": data
-        })
+            "message": "Task created successfully",
+            "task_id": task.id
+        }, status=201)
 
     elif request.method == 'PUT':
         data = json.loads(request.body)
 
-        return JsonResponse({
-            "message": "PUT request received",
-            "data": data
-        })
+        task_id = data.get('id')
+
+        try:
+            task = Task.objects.get(id=task_id)
+
+            task.title = data.get('title', task.title)
+            task.description = data.get(
+                'description',
+                task.description
+            )
+            task.status = data.get(
+                'status',
+                task.status
+            )
+
+            task.save()
+
+            return JsonResponse({
+                "message": "Task updated successfully"
+            })
+
+        except Task.DoesNotExist:
+            return JsonResponse({
+                "message": "Task not found"
+            }, status=404)
 
     elif request.method == 'DELETE':
-        return JsonResponse({
-            "message": "DELETE request received"
-        })
+        data = json.loads(request.body)
+
+        task_id = data.get('id')
+
+        try:
+            task = Task.objects.get(id=task_id)
+            task.delete()
+
+            return JsonResponse({
+                "message": "Task deleted successfully"
+            })
+
+        except Task.DoesNotExist:
+            return JsonResponse({
+                "message": "Task not found"
+            }, status=404)
 
     return JsonResponse({
         "message": "Method not allowed"
